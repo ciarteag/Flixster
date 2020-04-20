edd93e750c615a5ea79a609b27f8688bc6708285
Carlos Arteaga <ciarteag@uci.edu>
2020-04-19 23:41:52 -0700

    Added the see more details about a movie feature and superhero tab bar view

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Carlos Arteaga on 4/15/20.
+//  Copyright © 2020 Carlos Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Carlos Arteaga on 4/19/20.
+//  Copyright © 2020 Carlos Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
